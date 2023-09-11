8e8f0f9d6e253b1b0a939962f6cb0d0dce67f1c1
Trojan-254 <samwuels254@gmail.com>
2023-09-11 11:01:13 +0000

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    This module returns the list of available attributes
+    and methods of an object
+"""
+
+
+def lookup(obj):
+    """function lists all attributes and methods of an object"""
+    return dir(obj)
