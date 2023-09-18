d550d2a1b1e285050ec270431867887ecfdfafa7
Trojan-254 <samwuels254@gmail.com>
2023-09-18 16:19:31 +0000

    Almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module contains the base class"""
+
+class Base:
+   """This is the base class"""
+
+   __nb_objects = 0
+
+   def __init__(self, id=None):
+       """Initializes a new base
+       Args:
+           id(int): identity of the new base
+       """
+
+       if id is not None:
+          self.id  = id
+       else:
+           Base.__nb_objects += 1
+           self.id = Base.__nb_objects
