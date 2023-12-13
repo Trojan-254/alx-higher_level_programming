23d95cdb3815044b0eb734ce9c101d928e171619
Trojan-254 <samwuels254@gmail.com>
2023-12-13 06:43:16 -0800

    Task 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- script that prints the full description of the first_table form the table hbtn_0c_0 in my MySQL server
+-- The database name will be passed as an argument of the mysql command
+-- not allowed to use DESCRIBE or EXPLAIN statements
+
+SHOW CREATE TABLE first_table
