94294cc1b95ea48bbd771da59a96ef8580f2727b
Trojan-254 <samwuels254@gmail.com>
2024-02-20 03:54:15 -0800

    semistandard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 
-const fileName = process.argv[2]
-const stringy = process.argv[3]
+const fileName = process.argv[2];
+const stringy = process.argv[3];
 
 fs.writeFile(fileName, stringy, 'utf8', (err) => {
   if (err) throw err;
