495f4ecc0f6b047c992ebc73ecc74a5380777ef8
Trojan-254 <samwuels254@gmail.com>
2024-02-19 23:35:44 -0800

    Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs')
+
+const fileName = process.argv[2]
+const stringy = process.argv[3]
+
+fs.writeFile(fileName, stringy, 'utf8', (err) => {
+  if (err) throw err;
+});
