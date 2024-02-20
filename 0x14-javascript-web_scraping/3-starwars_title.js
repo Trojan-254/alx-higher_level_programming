931601aceca149617c8624591226908e0dac0ae9
Trojan-254 <samwuels254@gmail.com>
2024-02-20 04:10:04 -0800

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,10 +2,11 @@
 
 const request = require('request');
 const filmID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${filmID}`;
 
-request.get(`https://swapi-api.alx-tools.com/api/films/${filmID}`, 'utf-8', (error, response) => {
-   if (error) throw error;
-   const data = JSON.parse(response);
-   const title = data.title;
-   console.log(`Title: ${title}`);
+request.get(url, (error, response, body) => {
+  if (error) throw error;
+  const data = JSON.parse(body);
+  const title = data.title;
+  console.log(`${title}`);
 });
