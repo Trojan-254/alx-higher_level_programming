a6652e898dcdd2ca84d300fe01fb25be0ce524c3
Trojan-254 <samwuels254@gmail.com>
2024-02-20 04:01:18 -0800

    semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2]
+const url = process.argv[2];
 
 request(url, (error, response) => {
+  if (error) throw error;
   console.log(`code: ${response.statusCode}`);
 });
-
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
 const request = require('request');
-const filmID = process.argv[2]
+const filmID = process.argv[2];
 
 request.get(`https://swapi-api.alx-tools.com/api/films/${filmID}`, 'utf-8', (error, response) => {
-    if (error) throw error;
-    const data = JSON.parse(response);
-	const title = data.title;
-	console.log(`Title: ${title}`)
+   if (error) throw error;
+   const data = JSON.parse(response);
+   const title = data.title;
+   console.log(`Title: ${title}`);
 });
-
-
