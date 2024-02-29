21d28c4e972876eda1f699ed8a7443aad44de706
Trojan-254 <samwuels254@gmail.com>
2024-02-29 03:58:41 -0800

    Update header

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,9 @@
+/*
+ * script that updates the text of the <header> element to New Header!!!
+ * when the user clicks on DIV#update_header
+ */
+$(document).ready(function () {
+  $('#update_header').on('click', function () {
+    $('header').text('New Header!!!');
+  });
+});
