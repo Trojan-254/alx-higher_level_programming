f5485f26c1da58d2949e18e83885d7d617fdb519
Trojan-254 <samwuels254@gmail.com>
2024-02-29 03:52:40 -0800

    List of elements

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,14 @@
+/*
+ * script that toggles the class of the <header> element
+ * when the user clicks on the tag DIV#toggle_header
+ */
+$(document).ready(function () {
+  $('#toogle_header').on('click', function () {
+    const header = $('header');
+    if (header.hasClass('red')) {
+      header.removeClass('red').addClass('green');
+    } else {
+      header.removeClass('green').addClass('red');
+    }
+  });
+});
